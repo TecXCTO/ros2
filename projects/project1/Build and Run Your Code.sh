0f51bc8ea54d0fc251a30969c9c1f9e7fb3839a4
TecXCTO <cto.tecx@gmail.com>
2026-08-20 09:20:25 +0530

    Create Build and Run Your Code.sh

---
+++ projects/project1/Build and Run Your Code.sh
@@ -0,0 +1,9 @@
+# Build and Run Your Code
+# Go to your workspace root, build your system, and link the configurations:
+
+cd ~/ros2/projects/project1/ros2_ws
+colcon build --packages-select tecx_robot_pkg
+source install/setup.bash
+# To run your code, open Terminal Terminal 1: To run your code, open Terminal Terminal 1:
+source ~/ros2/projects/project1/ros2_ws/install/setup.bash
+ros2 run tecx_robot_pkg talker
