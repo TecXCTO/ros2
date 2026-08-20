62c06f2d5c6922b98ae3e2f9aa8993299f65567f
TecXCTO <cto.tecx@gmail.com>
2026-08-20 09:21:35 +0530

    Update Build and Run script with new terminal instructions
    Added instructions for opening a new terminal and running the listener.

--- projects/project1/Build and Run Your Code.sh
+++ projects/project1/Build and Run Your Code.sh
@@ -7,3 +7,9 @@ source install/setup.bash
 # To run your code, open Terminal Terminal 1: To run your code, open Terminal Terminal 1:
 source ~/ros2/projects/project1/ros2_ws/install/setup.bash
 ros2 run tecx_robot_pkg talker
+# Open a completely new Terminal Terminal 2:
+
+
+
+## source ~/ros2_ws/install/setup.bash
+##ros2 run my_robot_pkg listener
